f8aac272df96cadd1bbd2ad26eeeaa12534e2a43
matterwin <matthewderwin23@gmail.com>
2024-04-05 01:24:03 -0500

    backtracking to simpler scanner design

--- 00_lexical/src/main.cpp
+++ 00_lexical/src/main.cpp
@@ -1,57 +1,59 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
-#include <pthread.h>
+#include <string>
 #include <vector>
 
 #include "lexer.h"
 #include "token.h"
 #include "sym.h"
 
+using namespace std;
+
 void* scanFile(void* arg);
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " <filename> not provided" << std::endl;
+    cerr << "Usage: " << argv[0] << " <filename> not provided" << endl;
     return 1;
   }
 
-  std::vector<pthread_t> threads;
   for (int i = 1; i < argc; i++) {
-    pthread_t tid;
-    if (pthread_create(&tid, NULL, scanFile, (void*)argv[i]) != 0) {
-      std::cerr << "Error creating thread" << std::endl;
-      return 1;
-    }
-    threads.push_back(tid);
+    scanFile(argv[i]);   
   }
 
-  for (pthread_t tid : threads) {
-    if (pthread_join(tid, NULL) != 0) {
-      std::cerr << "Error joining thread" << std::endl;
-      return 1;
-    }
-  }
+  // after scanning & generating tokens, pass the tokens to the parser 
 
   return 0;
 }
 
 void* scanFile(void* arg) {
   const char* filename = (const char*)arg;
-  std::ifstream in_file(filename);
+  ifstream in_file(filename);
   if (!in_file) {
-    std::cout << "Failed to open input file." << std::endl;
+    cout << "Failed to open input file: " << filename << endl;
     return NULL;
   }
+  cout << "*** Processing: " << filename << endl;
 
-  std::string buffer((std::istreambuf_iterator<char>(in_file)), std::istreambuf_iterator<char>());
+  string line;
+  int line_number = 0;
+  while (getline(in_file, line)) {
+      line_number++;
+      cout << "Line " << line_number << endl;
 
-  Lexer lexer(buffer);
-  Token tok;
-  do {
-    tok = lexer.nextToken();
-    std::cout << "Token: " << tok.getValue() << std::endl;
-  } while (tok.getType() != Sym::_EOF);
+      for (char c : line) {
+        cout << "Character: " << c << endl;
+      }
+  }
+
+  in_file.close();
+  
+  // Lexer lexer(buffer);
+  // Token tok;
+  // do {
+  //   tok = lexer.nextToken();
+  //   cout << "Token: " << tok.getValue() << endl;
+  // } while (tok.getType() != Sym::_EOF);
 
   return NULL;
 }
--- 00_lexical/test/math_ops.c
+++ 00_lexical/test/math_ops.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int a = 10;
-    int b = 5;
+  int a = 10;
+  int b = 5;
 
-    // Addition
-    int sum = a + b;
-    printf("Sum: %d\n", sum);
+  // Addition
+  int sum = a + b;
+  printf("Sum: %d\n", sum);
 
-    // Subtraction
-    int difference = a - b;
-    printf("Difference: %d\n", difference);
+  // Subtraction
+  int difference = a - b;
+  printf("Difference: %d\n", difference);
 
-    // Multiplication
-    int product = a * b;
-    printf("Product: %d\n", product);
+  // Multiplication
+  int product = a * b;
+  printf("Product: %d\n", product);
 
-    // Division
-    int quotient = a / b;
-    printf("Quotient: %d\n", quotient);
+  // Division
+  int quotient = a / b;
+  printf("Quotient: %d\n", quotient);
 
-    // Modulus
-    int remainder = a % b;
-    printf("Remainder: %d\n", remainder);
+  // Modulus
+  int remainder = a % b;
+  printf("Remainder: %d\n", remainder);
 
-    return 0;
+  return 0;
 }
 
